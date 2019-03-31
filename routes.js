0ccaa91a356146d0529f3e145a3f5e094c9faed8
Des Whelan <wheland6@tcd.ie>
2019-03-31 15:28:33 +1300

    add image links to data

--- routes.js
+++ routes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const teamData = require('./teamData.json')
 
 router.get('/', function (req, res) {
-    res.send("hey ho let's go")
+
+    res.sendFile(__dirname + teamData["manchester united"][25]["picture"])
 })
 
 router.get('/starting-line-up', function (req, res) {
