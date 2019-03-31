1ebabf32d8cfbdad295b53a07c1112ba036399c2
Des Whelan <wheland6@tcd.ie>
2019-03-31 15:07:34 +1300

    set up routes

--- routes.js
+++ routes.js
@@ -3,6 +3,14 @@ const router = express.Router()
 
 router.get('/', function (req, res) {
     res.send("hey ho let's go")
-  })
+})
+
+router.get('/starting-line-up', function (req, res) {
+    res.send("here are 11 players")
+})
+
+router.get('/squad', function (req, res) {
+    res.send("here is the whole squad")
+})
 
 module.exports = router;
