9f6fde32764543e2c332bab0d2a97d7cdabb2d59
Des Whelan <wheland6@tcd.ie>
2019-03-31 17:56:56 +1300

    add randomizePlayers function

--- lineupSelector.js
+++ lineupSelector.js
@@ -7,6 +7,26 @@ function getPlayersByPosition (team, position) {
     })
 }
 
+function randomizePlayers(players) {
+    var currentIndex = players.length, temporaryValue, randomIndex;
+    
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+    
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+    
+        // And swap it with the current element.
+        temporaryValue = players[currentIndex];
+        players[currentIndex] = players[randomIndex];
+        players[randomIndex] = temporaryValue;
+    }
+    console.log("Randomized players: ", players)
+    return players
+}
+
 module.exports = {
-    getPlayersByPosition: getPlayersByPosition
+    getPlayersByPosition: getPlayersByPosition,
+    randomizePlayers: randomizePlayers
 }
