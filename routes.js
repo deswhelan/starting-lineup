4c53b08bbdee0a1352620051c7fd5df514044c90
Des Whelan <wheland6@tcd.ie>
2019-04-19 18:36:11 +1200

    make starting line-up the homepage

--- routes.js
+++ routes.js
@@ -5,19 +5,6 @@ const fs = require('fs')
 const pickAStartingEleven = require('./lineupSelector').pickAStartingEleven
 
 router.get('/', function (req, res) {
-    res.redirect('/squad')
-})
-
-router.get('/squad', function (req, res) {
-    fs.readFile('./teamData.json', 'utf8', function (err, data) {
-        if (err) {
-            return res.status(500).send('An Error Occured!')
-        }
-        res.render('players/squad', squads)
-    })
-})
-
-router.get('/starting-line-up', function (req, res) {
     fs.readFile('./teamData.json', 'utf8', function (err, data) {
         if (err) {
             return res.status(500).send('An Error Occured!')
